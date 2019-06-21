b3c03dfc164e937c7ee90bf801b3e485bad6558d
nirav <nrvshah06@gmail.com>
2019-06-21 01:14:29 -0400

    format and push again

--- src/Components/SideMenu/SideMenu.js
+++ src/Components/SideMenu/SideMenu.js
@@ -10,16 +10,15 @@ class SideMenu extends React.Component {
         }
     }
 
-    onClickItem(item)
-    {
+    onClickItem(item) {
         this.setState({ showNav: false });
         this.props.clickedItem(item);
     }
     render() {
-const item=!localStorage.getItem('user') ? <div onClick={()=>this.onClickItem("login")}>Login</div>:<div onClick={()=>this.onClickItem("logout")}>logout</div>;
+        const item = !localStorage.getItem('user') ? <div onClick={() => this.onClickItem("login")}>Login</div> : <div onClick={() => this.onClickItem("logout")}>logout</div>;
 
         return (
-            <div style={{marginTop: 25}}>
+            <div style={{ marginTop: 25 }}>
 
                 <MenuIcon onClick={() => this.setState({ showNav: true })} />
 
@@ -29,10 +28,10 @@ const item=!localStorage.getItem('user') ? <div onClick={()=>this.onClickItem("l
                     title="Menu"
                     items={[item,
                         <a href="/">Home</a>,
-                    
-                    ]} 
-                    titleStyle={{ color: 'black',backgroundColor:'grey' }}
-                    itemStyle={{ color: 'black',backgroundColor:'lightgrey' }}
+
+                    ]}
+                    titleStyle={{ color: 'black', backgroundColor: 'grey' }}
+                    itemStyle={{ color: 'black', backgroundColor: 'lightgrey' }}
                     itemHoverStyle={{ color: 'red' }}
 
 
